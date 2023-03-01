db2e5042188e3da759fe0a6fa09453a5b617e54e
AdithyaKathi <113934881+AdithyaKathi@users.noreply.github.com>
2023-03-01 16:07:37 +0530

    Add files via upload

---
+++ MyApp.java
@@ -0,0 +1,10 @@
+package com.mynotepad;
+/*
+  **********  Author : Adithya Kathi  *********
+ */
+public class MyApp {
+    public static void main(String[] args) {
+        NotePad notePad = new NotePad();
+    }
+
+}
